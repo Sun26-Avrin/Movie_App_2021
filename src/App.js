def519529f73ade358ebc72f63e2c3a51b7ded5b
HeeSun You <sim2626@naver.com>
2021-08-16 17:53:55 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state ={
+    isLoading : true,
+    movies : [],
+
+  } // 미래에 쓸변수를 미리 선언하는 공간이아님.
+
+  getMoives = async() =>{
+    // const moives = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    // console.log(moives.data.data.movies);
+    const {
+      data : {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies , isLoading:false});
+  };
+
+  componentDidMount(){ // fetch data from API
+    this.getMoives();
+  }
+
+
+
+  render(){
+    const {isLoading, movies} = this.state;
+
+    return  (
+    <section className ="container">
+      {isLoading 
+        ? (<div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div> 
+        ) : (
+          <div className="movies">
+            {movies.map(movie =>(
+                <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} 
+                genres={movie.genres}
+                />
+            ))}
+          </div>
+        
+        
+        
+        )}
+
+    </section>);
+  }
 }
 
 export default App;
+
